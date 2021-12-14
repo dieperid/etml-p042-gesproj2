dbfff605c2890d3689280c6fd2ea2b124ba6098e
Alexis1476 <halexisr20@hotmail.com>
2021-12-14 16:33:44 +0100

    register.php execute la requete de registre et affiche un popup

---
+++ web-site/src/php/lib/register.php
@@ -0,0 +1,47 @@
+<?php
+    // Fichier de connexion à la base de données
+    require('config.php');
+
+    // Declaration des variables, (recuperation de l'information mise dans le formulaire)
+    $cliNom = $_POST["cliNom"];
+    $cliPrenom = $_POST["cliPrenom"];
+    $cliNumTel = $_POST["cliNumTel"];
+    $cliLocalite = $_POST["cliLocalite"];
+    $cliCp = $_POST["cliCp"];
+    $cliUsername = $_POST["cliUsername"];
+    $cliPassword = $_POST["cliPassword"];
+    $cliAvatar = $_POST["cliAvatar"];
+
+    // Hachage du mot de passe
+    $hashPassword = password_hash($_POST['cliPassword'], PASSWORD_DEFAULT); 
+
+    // Enregistrement de l'information inserée dans le formulaire
+    try{
+    
+        // Requête preparée
+        $req = "INSERT INTO t_client "."(cliNom, cliPrenom, cliNumTel, cliLocalite, cliCp, cliUsername, cliPassword, cliAvatar)"." 
+        VALUES"."('$cliNom','$cliPrenom', '$cliNumTel', '$cliLocalite', '$cliCp', '$cliUsername','$hashPassword', '$cliAvatar')";
+
+        // Si la requête fonctionne
+        if($bdd->query($req) === TRUE)
+        {
+            function_alert("Bienvenue");
+        }
+        else
+        {
+            function_alert("Echec");
+        }  
+    }
+    catch(Exception $e)
+    {
+        die('Erreur : '.$e->getMessage());
+    }
+
+    // Affiche un popup d'alerte et redirecionne vers la page d'accueil
+    function function_alert($message) {     
+        echo "<script>
+        alert('$message');
+        window.location.href='../../html/index.html';
+        </script>";
+    }
+?>
