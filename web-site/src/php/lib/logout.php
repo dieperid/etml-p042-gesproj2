461d2585b4a2ef565889d7af725b336f7f74dd01
Alexis1476 <halexisr20@hotmail.com>
2021-12-17 13:05:02 +0100

    Code pour se déconnecter d'une session

---
+++ web-site/src/php/lib/logout.php
@@ -0,0 +1,15 @@
+<?php 
+session_start();
+
+// Suppression des variables de session et de la session
+$_SESSION = array();
+session_destroy();
+
+// Suppression des cookies de connexion automatique
+setcookie('login', '');
+setcookie('pass_hache', '');
+
+// Redirection vers la page d'accueil
+header('Location: ../../html/index.html');
+exit();
+?>
