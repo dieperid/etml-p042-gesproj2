f429824419b0d7002bd120be8d51b4e88736de44
dieperid <dieperid@sefanet.ch>
2021-12-17 14:31:29 +0100

    Implémentation du formulaire

---
+++ web-site/resources/js/formulaire.js
@@ -0,0 +1,20 @@
+//      MODAL    //
+
+//Get the modal element
+var modal = document.getElementById("simpleModal");
+var modal2 = document.getElementById("simpleModal2");
+
+//function to open modal 
+function openModal() {
+  modal.style.display = "block";
+}
+function openModal2() {
+  modal2.style.display = "block";
+}
+//function to close modal 
+function closeModal() {
+  modal.style.display = "none";
+}
+function closeModal2() {
+  modal2.style.display = "none";
+}
